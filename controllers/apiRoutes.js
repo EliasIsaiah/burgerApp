8fcf095a6f5767d0fb3898a16443a8088740eb60
Elias Roussos <eliasisaiah@outlook.com>
2019-10-01 20:57:39 -0400

    -index now populates with burgers

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -4,30 +4,31 @@ const Burger = require("../models/burger.js");
 // const friends = require("../data/friends.js")
 
 routes.post('/add', (req, res) => { //localhost:3000/api
-  
+
   console.log("body", req.body);
 
-  const {burger_name, devoured} = req.body;
-  
+  const { burger_name, devoured } = req.body;
+
   Burger.create({
     burger_name: burger_name,
     devoured: devoured
-  }).then(function(results) {
+  }).then(function (results) {
     res.json(results);
   }).catch(err => { throw err });
 });
 
-routes.get("/all", function(req, res) {
+routes.get("/all", function (req, res) {
 
   // Finding all Chirps, and then returning them to the user as JSON.
   // Sequelize queries are asynchronous, which helps with perceived speed.
   // If we want something to be guaranteed to happen after the query, we'll use
   // the .then function
-  Burger.findAll({}).then(function(results) {
-    // results are available to us inside the .then
-    res.json(results);
-  });
+  Burger.findAll({}).then(function (results) {
 
+    console.log(results);
+    res.json(results);
+
+  }).catch(err => { throw err });
 });
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set("view engine", "handlebars");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '..', 'burgerApp')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
