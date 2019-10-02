51e6266a6638bbe5efab7d65927b761718eaea85
Elias Roussos <eliasisaiah@outlook.com>
2019-10-01 20:57:46 -0400

    -index how populates with burgers

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,11 +1,17 @@
 const routes = require('express').Router();
 const path = require("path");
+const Burger = require("../models/burger.js");
 
 routes.get('/', (req, res) => {
 
   // res.status(200).json({ message: 'htmlRoutes Connected!' });
   // res.sendFile(path.join(__dirname, "..", "public", "home.html"));
-  res.render("index");
+  Burger.findAll({}).then(function (results) {
+
+    res.render("index", {burgers: results});
+
+  }).catch(err => { throw err });
+
 });
 
 module.exports = routes;
--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const uuid = require('uuid/v4');
 
 // Option 1: Passing parameters separately
 const sequelize = new Sequelize('burgerDatabase', 'root', 'root', {
@@ -19,16 +20,36 @@ const Model = Sequelize.Model;
 
 class Burger extends Model { }
 Burger.init({
-    burger_name: Sequelize.STRING,
-    devoured: Sequelize.BOOLEAN
+    uuid: {
+      allowNull: false,
+      primaryKey: true,
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4
+    },
+    burger_name: {
+      type: Sequelize.STRING
+    },
+    devoured: {
+      type: Sequelize.BOOLEAN
+    }
 }, { sequelize, modelName: 'burger' });
+
+// Burger.beforeCreate((burger, _ ) => {
+//   return burger.id = uuid();
+// });
+// return Burger;
 
 sequelize.sync({ force: true })
     .then(() => Burger.create({
         burger_name: 'cheeseburger',
         devoured: false
-    }))
-    .then(data => {
+    })).then(() => Burger.create({
+      burger_name: 'hamburger',
+      devoured: false
+  })).then(() => Burger.create({
+    burger_name: 'veggieburger',
+    devoured: false
+})).then(data => {
         console.log(data.toJSON());
     });
 
